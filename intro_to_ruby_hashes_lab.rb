1c77b820bbf4fc010efe83539c3a1702fca63374
fernando <fernando_lopezc@live.com>
2019-02-15 15:01:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
         b_and_o_railroad: {},
         shortline: {}
       }  #3rd tier of monopoly
-      rent_in_dollars: {
+      rent_in_dollars:{
         one_piece_owned: 25,
         two_pieces_owned: 50,
         three_pieces_owned: 100,
